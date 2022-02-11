20438b7c06b3b8e404a24f4087a1df64b1ed3057
Sahil <spatel@tvo.org>
2022-02-10 21:27:09 -0500

    Movies and movie by name api with schema and resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,8 +1,9 @@
-const { UserList } = require("../fakeData");
-const _  = require("lodash");
+const { UserList, MovieList } = require("../fakeData");
+const _ = require("lodash");
 
 const resolvers = {
   Query: {
+    // USER RESOLVERS
     users: () => {
       return UserList;
     },
@@ -11,6 +12,17 @@ const resolvers = {
       const id = args.id;
       const user = _.find(UserList, { id: Number(id) });
       return user;
+    },
+
+    // MOVIE RESOLVERS
+    movies: () => {
+      return MovieList;
+    },
+
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = _.find(MovieList, { name });
+      return movie;
     },
   },
 };
